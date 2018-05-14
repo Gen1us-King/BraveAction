ce9d18c7887550bc14b938a8fce56c19699c26e4
Gen1us-King <1205964772@qq.com>
2018-05-14 15:28:51 +0800

    No：1 场景切换

---
+++ BraveACtion/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [HideInInspector]
+    public Rigidbody2D playerRig;
+	// Use this for initialization
+	void Start () {
+        playerRig = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            playerRig.AddForce(Vector2.right*5f);
+        
+    }
+}
